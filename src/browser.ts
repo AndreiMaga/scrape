4b6525d3e01230e2677375f929b97f77966db649
Andrei Maga <andreimariomaga@gmail.com>
2021-01-17 18:26:41 +0200

    Removed puppeteer and using puppeteer-core

--- src/browser.ts
+++ src/browser.ts
@@ -1,12 +1,14 @@
-import { launch, Browser } from 'puppeteer'
-
+import { launch, Browser } from 'puppeteer-core'
+import { enumerateValues, HKEY, RegistryValueType } from 'registry-js'
+import { pathExists } from 'fs-extra'
 /**
  * Start a browser instance
  */
-export async function startBrowser(): Promise<Browser> {
+export async function startBrowser(exe_path: string): Promise<Browser> {
   try {
     let browser = await launch({
       headless: true,
+      executablePath: exe_path,
       args: ['--disable-setuid-sandbox'],
       ignoreHTTPSErrors: true
     })
@@ -16,4 +18,64 @@ export async function startBrowser(): Promise<Browser> {
     console.error(err)
   }
   return Promise.reject()
+}
+
+export async function findBrowser(): Promise<string | null> {
+  let firefox = await findFirefox()
+  let chrome = await findChrome()
+
+  if (chrome !== null) return chrome
+  if (firefox !== null) return firefox
+
+  return null
+}
+
+async function findFirefox(): Promise<string | null> {
+  let firefox = enumerateValues(
+    HKEY.HKEY_CLASSES_ROOT,
+    'Applications\\firefox.exe\\shell\\open\\command'
+  )
+  if (firefox.length === 0) {
+    return null
+  }
+
+  let first = firefox[0]
+
+  if (
+    first.type === RegistryValueType.REG_EXPAND_SZ ||
+    first.type === RegistryValueType.REG_SZ
+  ) {
+    let path = first.data.split('"')[1]
+
+    if (await pathExists(path)) {
+      return path
+    }
+  }
+
+  return null
+}
+
+async function findChrome(): Promise<string | null> {
+  let chrome = enumerateValues(
+    HKEY.HKEY_CLASSES_ROOT,
+    'ChromeHTML\\shell\\open\\command'
+  )
+  if (chrome.length === 0) {
+    return null
+  }
+
+  let first = chrome[0]
+
+  if (
+    first.type === RegistryValueType.REG_EXPAND_SZ ||
+    first.type === RegistryValueType.REG_SZ
+  ) {
+    let path = first.data.split('"')[1]
+
+    if (await pathExists(path)) {
+      return path
+    }
+  }
+
+  return null
 }
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,35 @@
 import { IScraperProps, Scraper } from './scraper'
 import { readdir, readFileSync } from 'fs'
-import { startBrowser } from './browser'
+import { findBrowser, startBrowser } from './browser'
 
 const scrapingConfigsDirectory = 'scrapingConfigs'
+findBrowser().then((browserPath) => {
+  if (browserPath === null) return
 
-startBrowser().then((browser) => {
-  readdir(scrapingConfigsDirectory, async (err, files) => {
-    await Promise.all(
-      files.map(async (files, index) => {
-        let str = readFileSync(scrapingConfigsDirectory + '/' + files, 'utf-8')
+  startBrowser(browserPath).then((browser) => {
+    readdir(scrapingConfigsDirectory, async (err, files) => {
+      await Promise.all(
+        files.map(async (files, index) => {
+          let str = readFileSync(
+            scrapingConfigsDirectory + '/' + files,
+            'utf-8'
+          )
 
-        let props: IScraperProps = JSON.parse(str)
-        props.browser = browser
-        props.newpage = index != 0
-        let res = await new Scraper(props).scrapeAllJobs()
+          let props: IScraperProps = JSON.parse(str)
+          props.browser = browser
+          props.newpage = index != 0
+          let res = await new Scraper(props).scrapeAllJobs()
 
-        if (res === undefined) {
-          return
-        }
+          if (res === undefined) {
+            return
+          }
 
-        res.forEach((e) => {
-          console.log(JSON.stringify(e))
+          res.forEach((e) => {
+            console.log(JSON.stringify(e))
+          })
         })
-      })
-    )
-    browser.close()
+      )
+      browser.close()
+    })
   })
 })
