28d2e426c6edcc70d48d17c603614733f598a7fc
Andrei Maga <andreimariomaga@gmail.com>
2021-01-18 15:54:02 +0200

    Added ignore.json

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,21 @@ import { findBrowser, startBrowser } from './browser'
 
 const scrapingConfigsDirectory = 'scrapingConfigs'
 const outputDirectory = 'results'
-
+const ignoreConfig = 'ignore.json'
 const fileName = new Date().getTime().toString()
+
+interface IIgnoredUrls {
+  urls: string[]
+}
 
 if (!existsSync(outputDirectory)) {
   mkdirSync(outputDirectory)
+}
+
+let ignored: IIgnoredUrls = { urls: [] }
+
+if (existsSync(ignoreConfig)) {
+  ignored = JSON.parse(readFileSync(ignoreConfig, 'utf-8'))
 }
 
 findBrowser().then((browserPath) => {
@@ -19,12 +29,11 @@ findBrowser().then((browserPath) => {
       let result: IResultProps[] = []
       await Promise.all(
         files.map(async (files, index) => {
-          let str = readFileSync(
-            scrapingConfigsDirectory + '/' + files,
-            'utf-8'
+          let props: IScraperProps = JSON.parse(
+            readFileSync(scrapingConfigsDirectory + '/' + files, 'utf-8')
           )
+          if (ignored.urls.includes(props.url)) return
 
-          let props: IScraperProps = JSON.parse(str)
           props.browser = browser
           props.newpage = index != 0
           let res: IResultProps[] | null = await new Scraper(
